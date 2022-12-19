6f46c121d93f5aabdfa8513972820fc248cd64f9
azzamdinabdillah <azzamdinabdillah123@gmail.com>
2022-12-20 05:41:30 +0700

    Updated-4

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'login' => 'App\\Http\\Livewire\\Login',
+  'posts' => 'App\\Http\\Livewire\\Posts',
+  'posts.comments' => 'App\\Http\\Livewire\\Posts\\Comments',
+  'posts.profile-users' => 'App\\Http\\Livewire\\Posts\\ProfileUsers',
+  'posts.profile-users-followers' => 'App\\Http\\Livewire\\Posts\\ProfileUsersFollowers',
+  'posts.profile-users-followings' => 'App\\Http\\Livewire\\Posts\\ProfileUsersFollowings',
+  'profile' => 'App\\Http\\Livewire\\Profile',
+  'register' => 'App\\Http\\Livewire\\Register',
+  'status.story' => 'App\\Http\\Livewire\\Status\\Story',
+  'users' => 'App\\Http\\Livewire\\Users',
+);
