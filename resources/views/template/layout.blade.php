5ef0d7f721389f8dd3683f5c5b988b4953b45d5d
azzamdinabdillah <azzamdinabdillah123@gmail.com>
2022-12-20 05:57:31 +0700

    Updated-6

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
+
+    <link rel="stylesheet" href="/build/assets/app.37c7543d.css">
 
     <script src="https://kit.fontawesome.com/9a3f20c4de.js" crossorigin="anonymous"></script>
 
